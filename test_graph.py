b16cd77eeb3a189c8a4fd23a531ecdedb3822f1b
Swithin Pais <swithin.pais@gmail.com>
2024-03-09 23:33:57 +0000

    Add testing using pytest

---
+++ test_graph.py
@@ -0,0 +1,74 @@
+from graph import Vertex, Edge, Graph
+
+
+def test_vertex():
+    v1 = Vertex("A")
+    v2 = Vertex("B")
+    v3 = Vertex("A")
+
+    assert v1 != v2
+    assert v1 == v3
+
+
+def test_edge():
+    v1 = Vertex("A")
+    v2 = Vertex("B")
+    v3 = Vertex("C")
+
+    e1 = Edge(v1, v2, "AB", 2)
+    e2 = Edge(v2, v3, "BC", 2)
+    e3 = Edge(v2, v1, "AB", 2)
+    e4 = Edge(v1, v2, "AB", 5)
+
+    assert e1 != e2
+    assert e3 == e1
+    assert e1 == e4
+
+    assert e1.opposite(v1) == v2
+    assert e2.opposite(v2) == v3
+
+
+def test_graph():
+    v1 = Vertex("A")
+    v2 = Vertex("B")
+    v3 = Vertex("C")
+
+    e1 = Edge(v1, v2, "AB", 2)
+    e2 = Edge(v2, v3, "BC", 2)
+    e3 = Edge(v1, v2, "AB", 5)
+    e4 = Edge(v2, v1, "AB", 2)
+
+    g = Graph()
+
+    g.add_vertex(v1)
+    g.add_vertex(v2)
+    g.add_vertex(v3)
+
+    g.add_edge(v1, v2, e1)
+    g.add_edge(v2, v3, e2)
+    g.add_edge(v1, v2, e3)
+    g.add_edge(v2, v1, e4)
+
+    assert len(g.vertices) == 3
+
+    assert len(g.edges) == 2  # 2 edges as e1 e3 and e4 are the same
+
+    assert g.num_vertices() == 3
+
+    assert g.num_edges() == 2
+
+    assert g.degree(v2) == 2
+
+    assert g.get_edge(v1, v2) == e1
+
+    assert g.get_edges(v2) == [e1, e2]
+
+    g.remove_edge(e2)
+
+    assert g.num_edges() == 1
+
+    g.remove_vertex(v2)
+
+    assert g.num_vertices() == 2
+
+    assert g.num_edges() == 0
---
+++ test_priority_queue.py
@@ -0,0 +1,68 @@
+from priority_queue import HeapAPQ, UnsortedListAPQ, APQ, Element
+
+import pytest
+
+
+@pytest.mark.parametrize("apq_class", [HeapAPQ, UnsortedListAPQ])
+def test_apq(apq_class: APQ):
+    apq: APQ = apq_class()
+
+    items = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+    for item in items:
+        e = apq.add(item, item)
+
+    assert apq.length() == 10
+
+    assert apq.min() == 0
+
+    assert apq.remove_min() == 0
+
+    apq.remove_min()
+    apq.remove_min()
+
+    assert apq.min() == 3
+
+    e2 = apq.add(1, 1)
+
+    assert apq.min() == 1
+
+    assert apq.get_key(e) == 9
+
+    assert apq.remove(e) == (9, 9)
+
+    apq.update_key(e2, 10)
+
+    assert apq.min() == 3
+
+    apq.update_key(e2, 0)
+
+    assert apq.min() == 1
+
+    apq = apq_class()
+
+    with pytest.raises(IndexError):
+        apq.remove_min()
+
+    with pytest.raises(IndexError):
+        apq.remove(e)
+
+
+def test_apq_abc():
+    with pytest.raises(TypeError):
+        apq = APQ()
+
+
+def test_element():
+    e1 = Element(1, 1, 1)
+
+    e2 = Element(1, 2, 2)
+
+    assert e1 == e2
+
+    e3 = Element(2, 3, 3)
+
+    assert e2 < e3
+
+    with pytest.raises(AttributeError):
+        e3.new_attr = 5
